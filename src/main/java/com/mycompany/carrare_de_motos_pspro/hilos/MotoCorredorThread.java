4147a5808a601687faf3bb1dc241c31b0b8f0aa1
mininekos <manuel2g2m@gmail.com>
2023-01-24 12:52:25 +0100

    V1.5

--- src/main/java/com/mycompany/carrare_de_motos_pspro/hilos/MotoCorredorThread.java
+++ src/main/java/com/mycompany/carrare_de_motos_pspro/hilos/MotoCorredorThread.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.carrare_de_motos_pspro.hilos;
 
+import com.mycompany.carrare_de_motos_pspro.pojos.Corredor;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
@@ -15,39 +17,57 @@ import javax.swing.JProgressBar;
 public class MotoCorredorThread extends Thread{
 
     Random rnd;
-    String name;
+    private Corredor corredor;
     JProgressBar barra;
     JLabel lbl;
     int vuelta;
     int avance;
     private final int MAXVUELTA=5;
-    public MotoCorredorThread(String name,JProgressBar barra, JLabel lbl) {
+    private boolean stop;
+    private ArrayList<Corredor> corredores;
+    public MotoCorredorThread(Corredor corredor,JProgressBar barra, JLabel lbl,ArrayList<Corredor> corredores ) {
         
-        this.name=name;
+        this.corredor=corredor;
         this.barra=barra;
         rnd=new Random();
         vuelta=0;
         this.lbl=lbl;
         lbl.setText(vuelta+"/"+MAXVUELTA);
+        stop=false;
+        this.corredores=corredores;
     }
 
+    public void pausar(){
+        stop=true;
+    }
+    
+    public synchronized void reanudar(){
+        stop=false;
+        notify();
+    }
+    
     @Override
-    public void run() {
+    public synchronized void run() {
         
         try{
             while(vuelta<MAXVUELTA){
-                
-                avance+=rnd.nextInt(1,3);
-                barra.setValue(avance);
-                if(barra.getValue()==100){
-                    barra.setValue(0);
-                    avance=0;
-                    vuelta++;
-                    lbl.setText(vuelta+"/"+MAXVUELTA);
-                }  
-                sleep(rnd.nextInt(10,100));
+                if(stop==false){  
+                    avance+=rnd.nextInt(1,5);
+                    barra.setValue(avance);
+                    if(barra.getValue()==100){
+                        barra.setValue(0);
+                        avance=0;
+                        vuelta++;
+                        lbl.setText(vuelta+"/"+MAXVUELTA);
+                    }  
+                    sleep(50);
+                }
+                else{
+                    wait();
+                }
             }
-                
+               barra.setValue(100);
+               corredores.add(corredor);
               
         }
         catch(Exception e){
--- src/main/java/com/mycompany/carrare_de_motos_pspro/pojos/Corredor.java
+++ src/main/java/com/mycompany/carrare_de_motos_pspro/pojos/Corredor.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.carrare_de_motos_pspro.pojos;
 
+import java.util.Objects;
+
 /**
  *
  * @author Dam
@@ -11,4 +13,42 @@ package com.mycompany.carrare_de_motos_pspro.pojos;
 public class Corredor {
     
     private String nombre;
+
+    public Corredor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.nombre);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Corredor other = (Corredor) obj;
+        return Objects.equals(this.nombre, other.nombre);
+    }
+    
+    
+    
+    
 }
